9fa059280820582261e41356a1c4623fb2d85417
William Nick <wmnick@aggies.ncat.edu>
2021-07-04 00:04:33 -0400

    refactor register.vue and the corresponding test

--- test/unit/specs/registration.spec.js
+++ test/unit/specs/registration.spec.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Register from '@/components/registration'
+import axios from 'axios'
+
+jest.mock('axios')
 
 describe('registration.vue', () => {
-  it('', () => {
+  it('Register should give rdf', () => {
     const Constructor = Vue.extend(Register)
     const vm = new Constructor().$mount()
   })
